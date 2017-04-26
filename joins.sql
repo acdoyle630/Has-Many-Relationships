7651d6cc6b08553b23474d6cf9e3f7a92050ce31
Adam <acdoyle630@gmail.com>
2017-04-26 11:38:47 -1000

    return all posts id for a specified user by first and last name

--- joins.sql
+++ joins.sql
@@ -5,7 +5,13 @@
 --   FROM posts
 --   WHERE users_id = 100;
 
-SELECT *
-  FROM users, posts
-  WHERE users.id = posts.users_id AND
-  users.id = 200;
+-- SELECT *
+--   FROM users, posts
+--   WHERE users.id = posts.users_id AND
+--   users.id = 200;
+
+SELECT posts.id, posts.title, posts.url, posts.content, posts.created_at, posts.updated_at, users.username
+  FROM posts, users
+  WHERE users.first_name = 'Norene' AND
+  users.last_name = 'Schmitt' AND
+  users.id = posts.users_id;
