1e0fa4eb2472982a6634a4a20fbfd2ea171a1290
Adam <acdoyle630@gmail.com>
2017-04-26 11:29:45 -1000

    join users and posts by id

--- joins.sql
+++ joins.sql
@@ -1,6 +1,11 @@
 -- SELECT *
 --   FROM users;
 
+-- SELECT *
+--   FROM posts
+--   WHERE users_id = 100;
+
 SELECT *
-  FROM posts
-  WHERE users_id = 100;
+  FROM users, posts
+  WHERE users.id = posts.users_id AND
+  users.id = 200;
