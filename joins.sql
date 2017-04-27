399660ff555673053ebbd4541bb4c33589781f4a
Adam <acdoyle630@gmail.com>
2017-04-26 14:24:35 -1000

    selected commenter username inside select statment,  MVP complete

--- joins.sql
+++ joins.sql
@@ -59,13 +59,37 @@
 --   WHERE comments.body LIKE '%USB%';
 
 --11
-SELECT title AS "post_title", users.first_name, users.last_name, comments.body AS "comment_body"
-  FROM posts
-  JOIN users
+-- SELECT title AS "post_title", users.first_name, users.last_name, comments.body AS "comment_body"
+--   FROM posts
+--   JOIN users
+--   ON posts.users_id = users.id
+--   JOIN comments
+--   ON comments.users_id = users.id
+--   WHERE comments.body LIKE '%matrix%';
+
+--12
+-- SELECT users.first_name, users.last_name, comments.body as comment_body
+-- FROM comments
+-- INNER JOIN posts
+-- ON comments.posts_id = posts.id
+-- INNER JOIN users
+-- ON posts.users_id = users.id
+-- WHERE comments.body LIKE '%SSL%' AND
+--   posts.content LIKE '%dolorum%';
+
+--13
+SELECT users.first_name AS "post_author_first_name", users.last_name AS "post_author_last_name", posts.title AS "Post_title", (
+  SELECT users.username
+    FROM users
+    WHERE comments.users_id = users.id)
+  AS "comment_author_username",
+  comments.body AS "comment_body"
+
+  FROM users
+  INNER JOIN posts
   ON posts.users_id = users.id
-  JOIN comments
-  ON comments.users_id = users.id
-  WHERE comments.body LIKE '%matrix%';
-
-
-
+  INNER JOIN comments
+  ON posts.id = comments.posts_id
+  WHERE (comments.body LIKE '%SSL%' OR
+    comments.body LIKE '%firewall%') AND
+    posts.content LIKE '%nemo%';
